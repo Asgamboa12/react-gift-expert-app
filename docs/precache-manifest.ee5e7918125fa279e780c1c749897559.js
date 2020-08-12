d597f82fbb7c69916d8f65fe1075d00d86386732
Asgamboa12 <33297469+Asgamboa12@users.noreply.github.com>
2020-08-12 14:05:36 -0600

    docs agregado

---
+++ docs/precache-manifest.ee5e7918125fa279e780c1c749897559.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69910e05f84f42be028ab2542c249f74",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35082765fab650fd3c24",
+    "url": "/static/css/main.ef0a187c.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35082765fab650fd3c24",
+    "url": "/static/js/main.1f42d7e8.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
